9cb235dd5cd952cad529180b4afbccf07f832015
codingkiddo <codingkiddo@gmail.com>
2024-03-29 19:00:25 +0530

    eurekaserver

--- src/main/java/com/optimagrowth/eureka/EurekaServerApplication.java
+++ src/main/java/com/optimagrowth/eureka/EurekaServerApplication.java
@@ -1,5 +1,7 @@
 package com.optimagrowth.eureka;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -9,6 +11,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
+		AtomicInteger atomicInteger;
+		
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
