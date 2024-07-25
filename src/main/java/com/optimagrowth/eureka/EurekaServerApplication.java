f638ed5ac65a6106245e0d71445fda92f78f7899
codingkiddo <codingkiddo@gmail.com>
2024-07-25 12:36:28 +0530

    modified:   src/main/resources/bootstrap.yml

--- src/main/java/com/optimagrowth/eureka/EurekaServerApplication.java
+++ src/main/java/com/optimagrowth/eureka/EurekaServerApplication.java
@@ -1,7 +1,5 @@
 package com.optimagrowth.eureka;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -11,8 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		AtomicInteger atomicInteger;
-		
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
